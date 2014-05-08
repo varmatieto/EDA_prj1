d6019d9f3915a96a8c1adf459222d921b98e4166
varmatieto <ninobixio22@gmail.com>
2014-05-08 08:06:50 +0200

    basic commit

--- plottingprj1.R
+++ plottingprj1.R
@@ -1,3 +1,4 @@
+# the file contains ONLY the two days under focus
 
 epc<-read.table("household_power_consumption.txt", sep=";",header=T,
                 stringsAsFactors = F)
@@ -44,5 +45,7 @@ with (epc, lines(myT,Sub_metering_2, type="l", col = "red"))
 with (epc, lines(myT,Sub_metering_3, type="l", col = "Blue"))
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
         pch = 1, col = c("black","red", "blue" )  )
+
+
 
 dev.off()
