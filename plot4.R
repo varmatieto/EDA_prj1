68e9877ff3133413b40bd739cd29b1ab460306ea
varmatieto <ninobixio22@gmail.com>
2014-05-08 17:52:40 +0200

    last plots
    an error in plot 3 and a new 4

--- plot4.R
+++ plot4.R
@@ -11,14 +11,26 @@ myT<-strptime(myTime, "%d/%m/%Y %H:%M:%S")
 
 
 
-png("plot3.png")
+
+
+png("plot4.png")
+par (mfrow=c(2,2))
+
+with (epc, plot(myT,Global_active_power, type="l",
+                ylab="Global Active Power", xlab=""   ))
+
+with (epc, plot(myT,Voltage, type="l",
+                xlab="datetime",ylab="Voltage"))
 
 with (epc, plot(myT,Sub_metering_1, type="l",
                 xlab="", ylab="Energy sub metering"))
 with (epc, lines(myT,Sub_metering_2, type="l", col = "red"))
 with (epc, lines(myT,Sub_metering_3, type="l", col = "Blue"))
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+legend("topright", legend = c("Sub_metering_1", 
+                              "Sub_metering_2", "Sub_metering_3"),
        pch = 151, col = c("black","red", "blue" )  )
 
+with (epc, plot(myT,Global_reactive_power, type="l",
+                xlab="datetime"))
 
 dev.off()
